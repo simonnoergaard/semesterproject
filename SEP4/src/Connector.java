b2ea62587b4c830f68e6fed3f9a2b0945dd9c3b5
camil <camil@192.168.87.164>
2020-05-06 13:46:21 +0200

    Connection

---
+++ SEP4/src/Connector.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+import java.util.ArrayList;
+//This class handles all interaction with the database
+public class Connector {
+	
+	private Connection connection;
+
+	
+	public void connect(int portNo, String userName, String password) {
+		// Establishing a PostgreSQL database connection
+		String databaseUrl = "jdbc:postgresql://localhost:" + 5432 + "/dawojpvu";
+
+		try {
+			connection = DriverManager.getConnection(databaseUrl, "dawojpvu", "8EDrrPQHm-LXcJQZ6AtvYwZbam2TwubZ");
+			System.out.println("Connection established to: Solarpanel" + databaseUrl);
+		} 
+		catch (Exception exception) {
+			System.out.println("Connection failed");
+			exception.printStackTrace();
+		}
+	}
+	
+	public void close() {
+		// Close the connection
+		try {
+			connection.close();
+			System.out.println("Connection closed");
+		} catch (SQLException exception) {
+			System.out.println("Connection closing failed");
+			exception.printStackTrace();
+		}
+	}
+
+}
